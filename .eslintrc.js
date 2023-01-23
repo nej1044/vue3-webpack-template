a0a92ef25c5b49360abada9893a2d4a1f758252e
nej1044 <nej1044@naver.com>
2023-01-23 15:50:02 +0900

    chore: create template

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        // 'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-strongly-recommended',
+        // 'plugin:vue/vue3-recommended',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'vue/html-closing-bracket-newline': ['error',{
+            'singleline': 'never',
+            'multiline': 'never'
+        }],
+        'vue/html-self-closing': ['error', {
+            'html': {
+                'void': 'always',
+                'normal': 'never',
+                'component': 'always',
+            },
+            'svg': 'always',
+            'math': 'always'
+        }]
+    }
+}
